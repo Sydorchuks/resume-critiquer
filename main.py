227c0be18086d5254ea6ca29408a0b06e6d437ef
Sydorchuks <77363409+Sydorchuks@users.noreply.github.com>
2025-06-11 12:07:41 +0300

    Basic program, just name

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import PyPDF2
+import io
+import os 
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="AI Resume Critiquer", page_icon = "", layout="centered")
+
+st.title("AI Resume Critiquer")
+st.markdown("Upload your resume and get AI-powered feedback tailored to your needs!")
+
